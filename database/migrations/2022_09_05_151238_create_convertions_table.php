97c735c8e1cdbce0cf07ca33bdc4b122be2fd28f
paulo <paulo@skilledpro.co>
2022-09-05 14:39:30 -0300

    Tabs fixing

--- database/migrations/2022_09_05_151238_create_convertions_table.php
+++ database/migrations/2022_09_05_151238_create_convertions_table.php
@@ -18,15 +18,15 @@ class CreateConvertionsTable extends Migration
             $table->string('from')->default('BRL')->nullable();
             $table->string('to')->nullable();
             $table->string('payment_method')->nullable();
-            $table->float('amount', 15,5)->nullable();
-            $table->float('tax_value', 15,5)->nullable();
-            $table->float('extra_tax_value', 15,5)->nullable();
-            $table->float('convertion_rate', 15,5)->nullable();
-            $table->float('total_converted_value', 15,5)->nullable();
-            $table->float('payment_tax', 15,5)->nullable();
-            $table->float('convertion_tax', 15,5)->nullable();
-            $table->float('discounted_amount', 15,5)->nullable();
-            $table->float('converted_discounted_amount', 15,5)->nullable();
+            $table->float('amount', 15, 5)->nullable();
+            $table->float('tax_value', 15, 5)->nullable();
+            $table->float('extra_tax_value', 15, 5)->nullable();
+            $table->float('convertion_rate', 15, 5)->nullable();
+            $table->float('total_converted_value', 15, 5)->nullable();
+            $table->float('payment_tax', 15, 5)->nullable();
+            $table->float('convertion_tax', 15, 5)->nullable();
+            $table->float('discounted_amount', 15, 5)->nullable();
+            $table->float('converted_discounted_amount', 15, 5)->nullable();
             $table->timestamps();
         });
     }
